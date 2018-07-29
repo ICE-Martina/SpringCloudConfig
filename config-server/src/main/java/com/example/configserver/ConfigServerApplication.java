074d5ae208f5f1d9c06de7429014d4e2ae4dfc6e
ash-ali <xulinjie0105@gmail.com>
2018-07-29 21:58:16 +0800

    实现高可用的分布式配置中心(Spring Cloud Config)(Finchley版本)

--- config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -2,13 +2,17 @@ package com.example.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * config server 启动类
  */
 //@EnableConfigServer开启配置服务器的功能
 @EnableConfigServer
+@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
