b972863725aa7af8fd35b0db4510b2bc797a1858
ash-ali <xulinjie0105@gmail.com>
2018-07-29 16:53:03 +0800

    分布式配置中心(Spring Cloud Config)(Finchley版本)

---
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.example.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * config server 启动类
+ */
+//@EnableConfigServer开启配置服务器的功能
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
